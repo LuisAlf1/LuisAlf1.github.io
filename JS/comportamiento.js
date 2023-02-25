1e966cddef627025af4e488b8ac04b1283485224
Luis Perez <luisfph12@gmail.com>
2023-02-25 18:43:51 -0300

    Solo falta font

--- JS/comportamiento.js
+++ JS/comportamiento.js
@@ -1,3 +1,50 @@
-document.getElementById('hy').addEventListener('click', function () {
-        document. getElementsByClassName('data1').style.display = 'inline-block'
+document.getElementById('Info').addEventListener('click', function () {
+        document.getElementById ('ht').style.display = 'inline-block';
+        document.getElementById ('cierre').style.display = 'block';
+    })
+
+document.getElementById('cierre').addEventListener('click', function () {
+        document.getElementById ('ht').style.display = 'none';
+        document.getElementById ('cierre').style.display = 'none'
+    })  
+
+document.getElementById('Expe').addEventListener('click', function () {
+        document.getElementById ('Ex').style.display = 'inline-block';
+        document.getElementById ('IIcierre').style.display = 'block';
+
+    }) 
+
+document.getElementById('IIcierre').addEventListener('click', function () {
+        document.getElementById ('Ex').style.display = 'none';
+        document.getElementById ('IIcierre').style.display = 'none'
+    })
+
+document.getElementById('Habi').addEventListener('click', function () {
+        document.getElementById ('habil').style.display = 'inline-block';
+        document.getElementById ('IIIcierre').style.display = 'block'
+    }) 
+
+document.getElementById('IIIcierre').addEventListener('click', function () {
+        document.getElementById ('habil').style.display = 'none';
+        document.getElementById ('IIIcierre').style.display = 'none'
+    })
+
+document.getElementById('Contac').addEventListener('click', function () {
+        document.getElementById ('Con').style.display = 'inline-block';
+        document.getElementById ('IVcierre').style.display = 'block'
+    }) 
+
+document.getElementById('IVcierre').addEventListener('click', function () {
+        document.getElementById ('Con').style.display = 'none';
+        document.getElementById ('IVcierre').style.display = 'none'
+    })
+    document.getElementById('menu').addEventListener('click', function () {
+        document.getElementById ('ht').style.display = 'none';
+        document.getElementById ('cierre').style.display = 'none';
+        document.getElementById ('Ex').style.display = 'none';
+        document.getElementById ('IIcierre').style.display = 'none';
+        document.getElementById ('habil').style.display = 'none';
+        document.getElementById ('IIIcierre').style.display = 'none';
+        document.getElementById ('Con').style.display = 'none';
+        document.getElementById ('IVcierre').style.display = 'none'
     })
